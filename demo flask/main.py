09cea25ca5fa78ffbed8d5cab2fc9169cde3f87b
Jaganmohan Reddy Sanikommu <69500541+jagansanikommu@users.noreply.github.com>
2023-03-10 15:01:03 +0530

    Add files via upload

--- demo flask/main.py
+++ demo flask/main.py
@@ -1,15 +1,26 @@
 from flask import Flask, render_template, request, redirect, url_for
 app=Flask(__name__)
+user={"jagan":"123456"}
 
 @app.route('/',methods={"GET","POST"})
+@app.route('/login',methods={"GET","POST"})
 def login():
     error = None
     if request.method == 'POST':
-        if request.form['username'] != 'jagan' or request.form['password'] != '123456':
-            error = 'Invalid Credentials. Please try again.'
+        if request.form['username'] not in user or request.form['password'] != user[request.form['username']]:
+            error="Invalid Credentials"
         else:
-            return render_template("Success.html")
-    return render_template('index.html', error=error)
+            details="Hello! "+request.form['username']
+            return render_template("Success.html",details=details)
+    return render_template('login.html', error=error)
+
+@app.route('/register',methods={"GET","POST"})
+def register():
+    error = None
+    if request.method == 'POST':
+        user[request.form['username']]=request.form['password']
+        return render_template('rsuccess.html')
+    return render_template('register.html', error=error)
 
 
 if __name__=="__main__":
